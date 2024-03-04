45b84857b735b3d02ea69af3d10415979aa8ec11
quyentt <quyentt1985@gmail.com>
2024-03-05 00:23:33 +0700

    up modul v2

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -39,6 +39,7 @@ $(document).ready(function () {
   // upload image vs video
   const imgVideoUpload = document.querySelectorAll(".image-selector");
   let videoSrc;
+
   imgVideoUpload.forEach((item) => {
     const fileUpload = item.querySelector(".file-input");
     const fileView = item.querySelector(".file-prev");
@@ -115,6 +116,118 @@ $(document).ready(function () {
       handleSetColours(picker, text);
     });
   });
+  // ------------select custom ----------
+  var x, i, j, l, ll, selElmnt, a, b, c;
+  /*Tìm class "custom-select":*/
+  x = document.getElementsByClassName("custom-select");
+  l = x.length;
+  for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*Tạo DIV đóng vai trò là mục chọn:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* Tạo DIV chứa danh sách option*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+      /*Tạo DIV chứa 1 option*/
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function (e) {
+        /*Cập nhật  "selected" khi click*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+      /*Đóng mở danh sách option*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
 
-  // end
+  function closeAllSelect(elmnt) {
+    /*Đóng mục chọn khi selected:*/
+    var x,
+      y,
+      i,
+      xl,
+      yl,
+      arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i);
+      } else {
+        y[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < xl; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide");
+      }
+    }
+  }
+  /*Đóng mục chọn khi click ra ngoài:*/
+  document.addEventListener("click", closeAllSelect);
+  // auto
+  const featureEle = document.querySelectorAll(" .feature-box");
+
+  for (let i = 0; i < featureEle.length; i++) {
+    let randomNum = Math.floor(Math.random() * 10 + 1);
+
+    featureEle[i].setAttribute("data-bg", randomNum);
+  }
+
+  // show quantity modul on page responsive!
+  const modulList = document.querySelector(".modul-v2 .modul-list");
+  const modulItem = document.querySelectorAll(".modul-v2 .modul-item");
+
+  console.log(modulItem[0].offsetWidth);
+  /*Lấy chiều rộng list modul*/
+  const modulListW = modulList.offsetWidth;
+  /*Số lượng modul có thể hiển thị trên 1 dòng*/
+  const modulItemPerRow = Math.floor(modulListW / 220);
+  /*Lấy số lượng modul*/
+  const modulItemQuantity = modulItem.length;
+  /*Hiển thị modul trên 1 dòng, ẩn phần còn lại*/
+  for (let i = 0; i < modulItemPerRow; i++) {
+    modulItem[i].classList.add("modul-show");
+  }
+  const modulViewMore = document.querySelector(".modul-view-more");
+  if (modulItemQuantity > modulItemPerRow) {
+    modulViewMore.classList.add("show");
+  }
+  modulViewMore.addEventListener("click", () => {
+    modulViewMore.classList.remove("show");
+    for (let i = 0; i <= modulItemQuantity; i++) {
+      modulItem[i].classList.add("modul-show");
+    }
+  });
 });
