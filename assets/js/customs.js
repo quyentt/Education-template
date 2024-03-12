3944b71c821a489c9cbcbf0df6cd0bea371d56d6
quyentt <quyentt1985@gmail.com>
2024-03-13 01:26:47 +0700

    1

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -384,6 +384,6 @@ $(document).ready(function () {
 
   //modal show test
   $(window).on("load", function () {
-    $("#deXuatHoiDongBaoVe_chitiet_lapHDmoi").modal("show");
+    // $("#deXuatHoiDongBaoVe_chitiet_lapHDmoi").modal("show");
   });
 });
