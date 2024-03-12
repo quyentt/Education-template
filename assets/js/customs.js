119ff71fee7bb1ee1948a2b95f4706c82709815a
quyentt <quyentt1985@gmail.com>
2024-03-12 12:39:30 +0700

    update duyet giao de tai

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -384,6 +384,6 @@ $(document).ready(function () {
 
   //modal show test
   $(window).on("load", function () {
-    $("#duyetGiaoDeTai_chitiet_duyet_dongy").modal("show");
+    // $("#duyetGiaoDeTai_chitiet_duyet_dongy").modal("show");
   });
 });
