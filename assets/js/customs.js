77e7a710510fcdf42c645bf8f237308e35c4e56b
quyentt <quyentt1985@gmail.com>
2024-03-18 11:53:27 +0700

    end kế hoạch năm học

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -384,6 +384,6 @@ $(document).ready(function () {
 
   //modal show test
   $(window).on("load", function () {
-    $("#khoaphancong_giangvien").modal("show");
+    // $("#khoaphancong_giangvien").modal("show");
   });
 });
