b3dc4f1c1b8aa09987f50d09c0ec2871f0fab804
quyentt <quyentt1985@gmail.com>
2024-03-23 17:37:21 +0700

    12

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -388,6 +388,6 @@ $(document).ready(function () {
 
   //modal show test
   $(window).on("load", function () {
-    // $("#khoaphancong_giangvien").modal("show");
+    $("#chinhsua_kehoach").modal("show");
   });
 });
